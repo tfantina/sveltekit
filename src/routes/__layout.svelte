3bf0af6e9a716ef9e9b24705bc12eb1647a2be72
Travis Fantina <travis@objectiveinc.com>
2021-11-05 19:20:53 -0700

    Initial sveltekit commit with some custom styling by me

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,66 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+	import 'bootstrap/dist/css/bootstrap.css';
+
+	import Scroller from '@sveltejs/svelte-scroller';
+
+	let index, offset, progress;
+</script>
+
+<Scroller top={0.2} bottom={5} bind:index bind:offset bind:progress>
+	<div slot="background">
+		<Header />
+
+		<main>
+			<slot />
+		</main>
+
+		<footer>
+			<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		</footer>
+	</div>
+
+	<div slot="foreground">
+		<img src="/images/fern-bg.png" class="photo" />
+	</div>
+</Scroller>
+
+<style>
+	.photo {
+		object-fit: cover;
+		top: 10;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,61 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from '$lib/Counter.svelte';
+	import Scroller from '@sveltejs/svelte-scroller';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>{index}</h1>
+{offset}
+{progress}
+<div class="page" />
+<h1>{index}</h1>
+{offset}
+{progress}
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.page {
+		height: 200vh;
+	}
+
+	.photo {
+		object-fit: cover;
+		top: 10;
+		left: 0;
+		width: 100%;
+		height: 100vh;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 100vh;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
